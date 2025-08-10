0183790c322631b0b943e0b8214971d080f69e0a
Oleksandr Yanytskyi <yanytskyi.oleksandr@gmail.com>
2025-08-10 12:27:39 +0300

    Driver

---
+++ Core/Driver/Driver.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+
+namespace Core.Driver
+{
+    public static class Driver
+    {
+        private static readonly ThreadLocal<IWebDriver?> webDriver = new();
+
+        public static IWebDriver? GetDriver()
+        {
+            return webDriver.Value;
+        }
+
+        public static void InitDriver(BrowserType browser = BrowserType.Chrome, bool headless = false, bool maximize = true)
+        {
+            if (webDriver.Value == null)
+            {
+                IWebDriver driver = WebDriverFactory.CreateDriver(browser, headless);
+                webDriver.Value = driver;
+
+                if (maximize)
+                {
+                    driver.Manage().Window.Maximize();
+                }
+            }
+        }
+
+        public static void QuitDriver()
+        {
+            webDriver.Value?.Quit();
+            webDriver.Value?.Dispose();
+            webDriver.Value = null;
+        }
+    }
+}
---
+++ Core/Driver/WebDriverFactory.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace Core.Driver
+{
+    public enum BrowserType
+    {
+        Chrome,
+        Firefox,
+        Edge,
+    }
+
+    public static class WebDriverFactory
+    {
+        public static IWebDriver CreateDriver(BrowserType browser, bool headless)
+        {
+            return browser switch
+            {
+                BrowserType.Chrome => CreateChromeDriver(headless),
+                BrowserType.Firefox => CreateFirefoxDriver(headless),
+                BrowserType.Edge => CreateEdgeDriver(headless),
+                _ => throw new ArgumentException("Unsupported browser type"),
+            };
+        }
+
+        private static ChromeDriver CreateChromeDriver(bool headless)
+        {
+            var options = new ChromeOptions();
+            if (headless) options.AddArgument("--headless");
+            return new ChromeDriver(options);
+        }
+
+        private static FirefoxDriver CreateFirefoxDriver(bool headless)
+        {
+            var options = new FirefoxOptions();
+            if (headless) options.AddArgument("--headless");
+            return new FirefoxDriver(options);
+        }
+
+        private static EdgeDriver CreateEdgeDriver(bool headless)
+        {
+            var options = new EdgeOptions();
+            if (headless) options.AddArgument("--headless");
+            return new EdgeDriver(options);
+        }
+    }
+}
