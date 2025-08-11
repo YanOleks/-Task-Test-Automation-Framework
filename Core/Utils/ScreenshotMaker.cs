a99545c7b0b4f747ad52a82a7e53d7cac0d26b72
Oleksandr Yanytskyi <yanytskyi.oleksandr@gmail.com>
2025-08-11 12:57:26 +0300

    ScreenshotMaker

---
+++ Core/Utils/ScreenshotMaker.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+using OpenQA.Selenium;
+
+namespace Core.Utils
+{
+    [SupportedOSPlatform("windows")]
+    [SupportedOSPlatform("linux")]
+    public static class ScreenshotMaker
+    {
+        private static string NewScreenshotName
+        {
+            get { return "_" + DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-fff") + "." + ScreenshotImageFormat; }
+        }
+        private static ImageFormat ScreenshotImageFormat
+        {
+            get { return ImageFormat.Jpeg; }
+        }
+        public static string TakeBrowserScreenshot(IWebDriver driver)
+        {
+            if (driver is not ITakesScreenshot takesScreenshot)
+            {
+                throw new ArgumentException("The provided WebDriver does not support taking screenshots.");
+            }
+
+            var screenshotPath = Path.Combine(Environment.CurrentDirectory, "Display" + NewScreenshotName);
+            var screenshot = takesScreenshot.GetScreenshot();
+            screenshot.SaveAsFile(screenshotPath);
+
+            return screenshotPath;
+        }
+    }
+}
